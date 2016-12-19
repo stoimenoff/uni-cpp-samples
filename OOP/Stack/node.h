2648cf31052fe5e77a0d7427ebb18f709ad4974b
stoimenoff <stoimenoffjr@gmail.com>
2016-12-19 20:56:05 +0200

    Simplify stack nodes

--- OOP/Stack/node.h
+++ OOP/Stack/node.h
@@ -8,39 +8,10 @@ class Node
 {
 
 	public:
-		Node(const T& data = NULL, Node<T>* next = NULL);
-		Node(const Node<T>& other);
-		Node<T>& operator = (const Node<T>& other);
-
+		Node(const T& data = NULL, Node<T>* next = NULL)
+			: data(data), next(next) {};
 		T data;
 		Node<T>* next;
-	private:
-		void init(const T& data, Node<T>* next);
 };
-
-template <typename T>
-void Node<T>::init(const T& data, Node<T>* next)
-{
-	this->data = data;
-	this->next = next;
-}
-
-template <typename T>
-Node<T>::Node(const T& data, Node<T>* next)
-{
-	init(data, next);
-}
-
-template <typename T>
-Node<T>::Node(const Node<T>& other)
-{
-	init(other.data, other.next);
-}
-
-template <typename T>
-Node<T>& Node<T>::operator = (const Node<T>& other)
-{
-	init(other.data, other.next);
-}
 
 #endif
