8f26097bd78e456b9cb090803263f3f50b513076
stoimenoff <stoimenoffjr@gmail.com>
2017-01-07 21:14:33 +0200

    Update Trees/Node structure to have parent

--- data-structures/trees/binary/node.h
+++ data-structures/trees/binary/node.h
@@ -7,9 +7,10 @@ struct Node
 	T data;
 	Node<T> *left;
 	Node<T> *right;
+	Node<T> *parent;
 
-	Node(const T& data, Node<T> *left = nullptr, Node<T> *right = nullptr)
-		: data(data), left(left), right(right) {}
+	Node(const T& data, Node<T> *left = nullptr, Node<T> *right = nullptr, Node<T> *parent = nullptr)
+		: data(data), left(left), right(right), parent(parent) {}
 };
 
 #endif // _NODE_H_
