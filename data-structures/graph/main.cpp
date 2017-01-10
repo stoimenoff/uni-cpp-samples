d6ec05ad82518d0ce6f7f178984724bec717b38d
stoimenoff <stoimenoffjr@gmail.com>
2017-01-10 18:54:00 +0200

    Remove huge graph from .ps

--- data-structures/graph/main.cpp
+++ data-structures/graph/main.cpp
@@ -31,20 +31,5 @@ int main()
 	for (const auto& entry : allBFSInducedGraphs(names))
 		entry.second.dottyPrint(std::cerr);
 
-	Graph<int> bigOne;
-	bigOne.addVertex(0);
-	for (int i = 1; i < 100; ++i)
-	{
-		bigOne.addVertex(i);
-		bigOne.addEdge(i, i-1);
-	}
-	bigOne.addEdge(0, 99);
-
-	for (int i = 0; i < 100; ++i)
-	{
-		bigOne.addEdge(i, 99-i);
-	}
-	bigOne.dottyPrint(std::cerr);
-
 	return 0;
 }
